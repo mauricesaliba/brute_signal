93408c257ebfa3409e18f513e5fcb42c3f576b82
Paulseph-John Farrugia <paulseph.farrugia@go.com.mt>
2019-11-24 01:56:00 +0100

    Added more information on connections found. Updated icon.

--- app/src/main/java/com/example/wifitutorial/WiFiReceiver.java
+++ app/src/main/java/com/example/wifitutorial/WiFiReceiver.java
@@ -22,12 +22,85 @@ public class WiFiReceiver extends BroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onReceive(Context context, Intent intent) {
-        List<ScanResult> scanResults = this.wiFiScannerActivity.wifiManager.getScanResults().stream().sorted((o1, o2) -> Integer.compare(o2.level, o1.level)).collect(Collectors.toList());
+        List<ScanResult> scanResults
+                = this.wiFiScannerActivity.wifiManager.getScanResults().stream()
+                    .sorted((o1, o2) -> Integer.compare(o2.level, o1.level))
+                        .collect(Collectors.toList());
         this.wiFiScannerActivity.unregisterReceiver(this);
 
         for (ScanResult scanResult : scanResults) {
-            this.wiFiScannerActivity.arrayList.add("SSID: " + scanResult.SSID + "\nLevel: " + scanResult.level + "dBm");
+            this.wiFiScannerActivity.arrayList.add(WiFiReceiver.formatScanResult(scanResult));
             this.wiFiScannerActivity.arrayAdapter.notifyDataSetChanged();
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private static String formatScanResult(ScanResult scanResult) {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("SSID: ");
+        stringBuffer.append(scanResult.SSID.length() > 0?scanResult.SSID:"<<Hidden Network>>");
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("BSSID: ");
+        stringBuffer.append(scanResult.BSSID);
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("Capabilities: ");
+        stringBuffer.append(scanResult.capabilities);
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("Channel Width: ");
+        stringBuffer.append(WiFiReceiver.formatChannelWidth(scanResult.channelWidth));
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("Frequency: ");
+        stringBuffer.append(scanResult.frequency);
+        stringBuffer.append(" MHz");
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("Strength Level: ");
+        stringBuffer.append(scanResult.level);
+        stringBuffer.append(" dBm");
+        stringBuffer.append(System.lineSeparator());
+
+        stringBuffer.append("Quality of Connection Here: ");
+        stringBuffer.append(WiFiReceiver.friendlyStrengthLevelIndicator(scanResult.level));
+        stringBuffer.append(System.lineSeparator());
+
+        return stringBuffer.toString();
+    }
+
+    private static String formatChannelWidth(int channelWidth) {
+        String string;
+
+        switch (channelWidth) {
+            case ScanResult.CHANNEL_WIDTH_20MHZ: string = "20 MHz"; break;
+            case ScanResult.CHANNEL_WIDTH_40MHZ: string = "40 MHz"; break;
+            case ScanResult.CHANNEL_WIDTH_80MHZ: string = "80 MHz"; break;
+            case ScanResult.CHANNEL_WIDTH_80MHZ_PLUS_MHZ: string = "80 MHz + 80 MHz"; break;
+            case ScanResult.CHANNEL_WIDTH_160MHZ: string = "160 MHz"; break;
+            default: string = "<<Unknown Channel Width>>"; break;
+        }
+
+        return string;
+    }
+
+    private static String friendlyStrengthLevelIndicator(int level) {
+        String string;
+
+        if (level >= -50) {
+            string = "Excellent";
+        } else if (level >= -60) {
+            string = "Good";
+        } else if (level >= -70) {
+            string = "Fair";
+        } else {
+            string = "Weak";
+        }
+
+        return string;
     }
 }
